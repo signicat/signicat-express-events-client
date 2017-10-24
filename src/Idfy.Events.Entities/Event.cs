f0eec0183ac0a445c9caa6441782451891d59746
Christoffer Slettan <christofferslettan@gmail.com>
2017-10-24 15:13:29 +0200

    Add base Event class

---
+++ src/Idfy.Events.Entities/Event.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Idfy.Events.Entities
+{
+    public interface IEvent
+    {
+        Guid Id { get; }
+        EventType Type { get; set; }
+        object Payload { get; set; }
+    }
+    
+    public abstract class Event<T> : IEvent where T : class
+    {
+        protected Event(EventType type, T payload)
+        {
+            Id = Guid.NewGuid();
+            Type = type;
+            Payload = payload;
+        }
+
+        public Guid Id { get; }
+        public EventType Type { get; set; }
+        public object Payload { get; set; }
+    }
+}
