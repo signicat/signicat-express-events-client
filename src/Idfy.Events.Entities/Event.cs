4531483b0785bf897977a9210daa24abe2a2c4d7
Christoffer Slettan <christofferslettan@gmail.com>
2017-10-30 09:33:36 +0100

    Update event with timestamp

--- src/Idfy.Events.Entities/Event.cs
+++ src/Idfy.Events.Entities/Event.cs
@@ -5,6 +5,7 @@ namespace Idfy.Events.Entities
     public abstract class Event
     {
         public Guid Id { get; protected set; }
+        public DateTime Timestamp { get; protected set; }
         public EventType Type { get; set; }
         public abstract object RawPayload { get; }
     }
@@ -14,6 +15,7 @@ namespace Idfy.Events.Entities
         protected Event(EventType type, T payload)
         {
             Id = Guid.NewGuid();
+            Timestamp = DateTime.UtcNow;
             Type = type;
             Payload = payload;
         }
